70110384fd4f908bc717323d42f180233aa698bd
Desyasaraswati <153419518+Desyasaraswati@users.noreply.github.com>
2025-01-14 19:08:07 +0800

    feat: remove root route

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/Road-to-bhakti/{id}', [DashboardController::class, 'index']);
