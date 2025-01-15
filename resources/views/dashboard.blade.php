bb38797b27dcc14c824c5a1f540da28449ac10e4
nining19 <eningsavitriii@gmail.com>
2025-01-15 16:24:06 +0800

    update bagian dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,7 +18,23 @@
      </div>
 
 <!-- section 2 -->
-
+<div class="container p-5">
+  <div class="shadow card rounded-3 p-5" >
+      <div class="row g-0">
+       <div class="col-md-4">
+          <img src="{{asset('images/LogoRTB.png')}}" class="img-fluid rounded-start " alt="Logo Road to Bhakti">
+       </div>
+       <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title" id="text-1">Road to Bhakti</h5>
+            <p class="card-text fs-4 text-justify">
+            Proyek Road to Bhakti bertujuan untuk mengembangkan sistem yang menyediakan fitur tutorial pembuatan banten serta katalog doa persembahyangan dan doa sehari-hari, untuk membantu serta mendukung masyarakat umat Agama Hindu untuk memahami dan melestarikan budaya sekaligus memperkuat keterhubungan umat dengan Tuhannya, sebagai bentuk implementasi dari Tri Hitakarana, yaitu "Parahyangan".
+          </p>
+          </div>
+       </div>
+      </div>
+  </div>
+</div>
 
 @endsection
 
