77b9c490c3e040f575322bb06c4beead3595e2c7
Desyasaraswati <153419518+Desyasaraswati@users.noreply.github.com>
2025-01-21 20:48:23 +0800

    update conf

--- app/Models/Pengguna.php
+++ app/Models/Pengguna.php
@@ -5,8 +5,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Pengguna extends Model
+class Pengguna extends Authenticatable
 {
     use HasFactory;
 
